8cc5305b385420ea643141fcdcc7d3ef877de868
Nassos Antoniou <nassos@thoughtworks.com>
2017-01-09 15:51:01 +0000

    Moving pipeline registration code inside correct context

--- domain/pipeline.go
+++ domain/pipeline.go
@@ -1,9 +1,48 @@
 package domain
 
+import (
+	"fmt"
+	"log"
+)
+
 type Pipeline interface {
 	Create([]string) error
 	Delete(interface{}) error
 	Read(interface{}) interface{}
 }
 
+// BasePipeline
+type BasePipeline struct {
+	Name  string
+	Steps []Actor
+}
+
 type PipelineFactory func(conf map[string]string) (Pipeline, error)
+
+var pipelineFactories = make(map[string]PipelineFactory)
+
+func Register(name string, factory PipelineFactory) {
+	if factory == nil {
+		log.Fatalf("Pipeline factory %s does not exist.", name)
+	}
+	_, registered := pipelineFactories[name]
+	if registered {
+		log.Fatalf("Pipeline factory %s already registered. Ignoring.", name)
+	}
+	pipelineFactories[name] = factory
+}
+
+func init() {
+	Register("AWS_CP", NewAWSCodePipeline)
+}
+
+func NewPipeline(conf map[string]string) (Pipeline, error) {
+	provider := conf["provider"]
+	pipelineFactory, ok := pipelineFactories[provider]
+
+	if !ok {
+		return nil, fmt.Errorf("Invalid pipeline type %s.", provider)
+	}
+
+	return pipelineFactory(conf)
+}
--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/antoniou/zero2Pipe/domain"
+	"github.com/antoniou/zero2Pipe/client"
 )
 
-var pipelineFactories = make(map[string]domain.PipelineFactory)
-
 func main() {
-	cli, _ := New()
-	cli.Run(os.Args)
-}
-
-func Register(name string, factory domain.PipelineFactory) {
-	if factory == nil {
-		log.Fatalf("Pipeline factory %s does not exist.", name)
-	}
-	_, registered := pipelineFactories[name]
-	if registered {
-		log.Fatalf("Pipeline factory %s already registered. Ignoring.", name)
-	}
-	pipelineFactories[name] = factory
-}
-
-func init() {
-	Register("AWS_CP", domain.NewAWSCodePipeline)
+	client.New().Run(os.Args)
 }
