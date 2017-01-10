74ff81a2970ce2e1f59c9d89fedb687465f6007d
Nassos Antoniou <nassos@thoughtworks.com>
2017-01-10 18:46:20 +0000

    Initialises Source resource with an authentication token

--- domain/source.go
+++ domain/source.go
@@ -1,7 +1,11 @@
 package domain
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -9,6 +13,7 @@ import (
 type Source interface {
 	Name() string
 	Owner() string
+	Auth() string
 }
 
 // PathSource implements Source and retrieves the Git source under the current path
@@ -16,13 +21,14 @@ type PathSource struct {
 	name  string
 	owner string
 	path  string
+	auth  string
 }
 
 func NewPathSource(path string) (Source, error) {
 	c := &PathSource{
 		path: path,
 	}
-	if err := c.resolve(); err != nil {
+	if err := c.init(); err != nil {
 		return nil, err
 	}
 
@@ -37,16 +43,43 @@ func (c *PathSource) Owner() string {
 	return c.owner
 }
 
+func (c *PathSource) Auth() string {
+	return c.auth
+}
+
 func (c *PathSource) resolve() error {
 	out, err := exec.Command("bash", "-c", fmt.Sprintf("set -e; cd %s;git remote get-url origin", c.path)).Output()
 	if err != nil {
 		return fmt.Errorf("Could not find repository under path %s: %s", c.path, err.Error())
 	}
-
 	url := strings.TrimSpace(string(out))
 	urlItems := strings.Split(url, "/")
 
 	c.owner = urlItems[len(urlItems)-2]
 	c.name = urlItems[len(urlItems)-1]
 	return nil
+}
+
+func (c *PathSource) authenticate() error {
+	// !!!!!!!!FIXME!!!!!!!!
+	buf := bytes.NewBuffer(nil)
+	f, err := os.Open("../.githubaccess")
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	io.Copy(buf, f)
+	c.auth = string(buf.Bytes())
+	return nil
+}
+
+func (c *PathSource) init() error {
+	err := c.resolve()
+	if err != nil {
+		return err
+	}
+
+	err = c.authenticate()
+	return err
 }
